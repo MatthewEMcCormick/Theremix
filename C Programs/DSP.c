0718c3bd28a0b4b9aa7b3b5a99921809314b50ae
Matt McCormick <mccorm62@purdue.edu>
2024-09-18 14:16:53 -0400

    Added code for saturator and Compressor. Not fully finished

--- C Programs/DSP.c
+++ C Programs/DSP.c
@@ -1,6 +1,6 @@
 #include <math.h>
 
-void saturator(int gainIn, int WD, int gainOut, double audio)
+double saturator(int gainIn, int WD, int gainOut, double audio)
 {
     int scaleVal = 10;
     double newAudioIn;
@@ -12,27 +12,53 @@ void saturator(int gainIn, int WD, int gainOut, double audio)
     //process in tan function 
     output = tanh(newAudioIn * WD/100);
     //multiple by outGain value
+    output = output* gainOut;
 
-    //scale back to output range
+    return output;
+}
 
+
+
+double compressor(double audio, int WD, double threshold, double attack, double release, double ratio, double gainOut, int * Activated, double * currentGR, int * releaseAct)
+{
+    double output;
+    //need to set activated to zero in the intialization call
+    if(Activated != 1 && audio > threshold)
+    {
+         * Activated = 1;
+    }
+    if (Activated == 1)
+    {
+        output = audio / *currentGR;
+       
+        if(* releaseAct == 0)
+        { 
+            if(*currentGR <= ratio)
+            {
+                *currentGR += (ratio/attack);
+            }
+            else if(*currentGR > ratio)
+            {
+                *currentGR = ratio;
+                *releaseAct = 1;
+            }
+        }
+        else
+        {
+            if(*currentGR > 1)
+            {
+                *currentGR -= (ratio/release);
+            }
+            else if(*currentGR < 1)
+            {
+                *currentGR = 1;
+                *releaseAct = 0;
+            }
+        }
+        
+    }
+    output = output * gainOut;
+
+    return output;
     
-}
-
-double gainAdjustment(int mode, double sample, double gain)
-{
-    //Gain In Adjustment 
-    if(mode == 0)
-    {
-        //add 
-    }
-    //Gain Out Adjustment 
-    else
-    {
-        //add
-    }
-}
-
-int dspMain(double audioIn, double WD, double gainIn, double gainOut, double threshold, double attack, double release, double ratio)
-{
-
 }
